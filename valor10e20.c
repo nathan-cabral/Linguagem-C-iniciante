290f04e1d319812fba6166cbd4a7749ed3870870
nathancabrall <nathaneliascabral@gmail.com>
2026-02-24 14:55:39 -0300

    Create valor10e20.c

---
+++ valor10e20.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+
+    int N,X,i,in=0,out=0;
+    
+    scanf("%d",&N);
+
+    for(i=0;i<=N-1;i++){
+        scanf("%d",&X);
+        if(X>=10 && X<=20){
+            in++;
+        }else{
+            out++;
+        }
+    }
+    printf("%d in\n"\
+    "%d out\n"\
+    ,in,out);
+
+    return 0;
+}
